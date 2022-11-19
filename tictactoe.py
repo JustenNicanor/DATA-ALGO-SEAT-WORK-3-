9d745deea16cb6525cb963fb780690933d496b3d
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2022-11-19 13:34:46 +0800

    Add files via upload
    "FIRST COMMIT" Making a board

---
+++ tictactoe.py
@@ -0,0 +1,12 @@
+board = [
+  ["_", "_", "_"],
+  ["_", "_", "_"],
+  ["_", "_", "_"]
+]
+
+def print_board(board):
+    for row in board:
+        for slot in row:
+            print(slot)
+
+print_board(board)
