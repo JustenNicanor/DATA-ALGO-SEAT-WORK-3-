cdd7f7ae3f8ad835d701a7a2930b5ddfac2e13c4
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2022-11-19 13:48:48 +0800

    "SECOND COMMIT"
    Asking user to quit or not then reply.

--- tictactoe.py
+++ tictactoe.py
@@ -7,6 +7,18 @@ board = [
 def print_board(board):
     for row in board:
         for slot in row:
-            print(slot)
+            print(f"{slot}", end="")
+        print()
 
 print_board(board)
+
+def quit(user_input):
+    if user_input == "q": 
+        print("Thanks for playing")
+        return True
+    else: return False
+
+while True:
+    user_input = input("Please enter a position using numbers from 1 to 9 or enter \"q\" to quit: ")
+    if quit(user_input): break
+    
