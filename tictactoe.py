b5cce92ffcffea840465e4f00145c639c573204c
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2022-11-19 14:51:59 +0800

    "FOURTH COMMIT"
    Making sure that the position can take place just once.

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,7 @@
 board = [
-  ["_", "_", "_"],
-  ["_", "_", "_"],
-  ["_", "_", "_"]
+  ["-", "-", "-"],
+  ["-", "-", "-"],
+  ["-", "-", "-"]
 ]
 
 def print_board(board):
@@ -19,8 +19,9 @@ def quit(userinput):
     else: return False
 
 def checkinput(user_input):
-    isnum(user_input)
-    
+    if not isnum(user_input):
+        return False
+    else: return True
 
 def isnum(userinput):
     if not userinput.isnumeric():
@@ -28,9 +29,31 @@ def isnum(userinput):
         return False
     else: return True
 
+def istaken(coords, board):
+    row = coords[0]
+    col = coords[1]
+    if board[row][col] != "-":
+        print("This position is taken already.")
+        return True
+    else: return False
+
+def coordinates(user_input):
+    row = int(user_input / 3)
+    col = user_input
+    if col > 2: col = int(col % 3)
+    return (row,col)
+
+
 while True:
+    print_board(board)
     user_input = input("Please enter a position using numbers from 1 to 9 or enter \"q\" to quit: ")
     if quit(user_input): break
     if not checkinput(user_input):
         print("Please try Again.")
+        continue
+    user_input = int(user_input) -1
+    coords = coordinates(user_input)
+    board[0][0] = "x"
+    if istaken(coords, board):
+        print("Please try again.")
         continue
